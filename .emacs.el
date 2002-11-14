ac3cd5ac1363041d1e8f818ce6e9943dbd4dbb01
Chunyu Wang <spr@db.hit.edu.cn>
2002-11-14 02:39:35 +0000

    new version.

--- .emacs.el
+++ .emacs.el
@@ -1,50 +1,50 @@
-(global-set-key [f1] 'cvs-examine)
-(global-set-key [f2] 'browse-url-lynx-emacs)
-(global-set-key [f3] 'eshell)
+;; $Id$  -*- Emacs-Lisp -*-
+;; Chunyu's .emacs.el is created on ??.    Chunyu.<dddkk@sina.com> 
+
+(global-set-key [f1] 'cvs-examine) 
+(global-set-key [f2] 'browse-url-lynx-emacs) 
+(global-set-key [f3] 'eshell) 
 (global-set-key [f4] 'kill-buffer)
 (global-set-key [f5] 'undo)
 (global-set-key [f6] 'ansi-term)
 (global-set-key [f7] 'calendar)
-(global-set-key [f8] 'other-window)
+(global-set-key [f8] 'other-window) 
 (global-set-key [f9] 'indent-region)
 (global-set-key [f10] 'man-follow)
 (global-set-key [f11] 'compile)
 (global-set-key [f12] 'gdb)
 
-(global-set-key "\C-co" 'occur)
 (global-set-key "\C-x\C-b" 'electric-buffer-list)
-(global-set-key "\C-c\C-b" 'login-bbs-by-name)
-(global-set-key [(control z)]  'set-mark-command)
-(global-set-key [(home)] 'beginning-of-buffer)
-(global-set-key [(end)]        'end-of-buffer)
-(global-set-key [(select)]        'end-of-buffer)
-(global-set-key [kp-delete] 'delete-char)
-(global-set-key [delete]    'delete-char)
+(global-set-key "\C-c\C-o" 'occur)
+(global-set-key [(control z)] 'set-mark-command)
+(global-set-key [(home)]   'beginning-of-buffer)
+(global-set-key [(end)]     'end-of-buffer)
+(global-set-key [(select)]  'end-of-buffer)
+;;(global-set-key [kp-delete] 'delete-char)
+;;(global-set-key [delete]    'delete-char)
 
 (setq-default transient-mark-mode nil)
 (setq-default truncate-lines nil)
-(setq-default save-place t)
-
-(setq text-mode-hook 'turn-on-auto-fill)
+;;(setq text-mode-hook 'turn-on-auto-fill)
 (setq default-major-mode 'text-mode)
 (setq display-time-day-and-date t)
 (setq next-line-add-newlines nil)
 (setq display-time-24hr-format t)
 (setq require-final-newline t)
+(setq user-mail-address "dddkk@sina.com")
+(setq dired-listing-switches "-avl")
+(setq user-full-name "Chunyu")
 (setq gdb-command-name "gdb ")
 (setq compile-command "make ")
-
+(setq diary-file "~/.diary")
 (setq scroll-step 0)
 ;; (prefer-coding-system 'chinese-iso-8bit)
 (auto-compression-mode t)
-(column-number-mode    t)
-(line-number-mode      t)
-(menu-bar-mode       nil)
-(show-paren-mode       t)
-(display-time-mode     t)
-(auto-revert-mode)
-
-;;(load-file "/home/spr/.emacs.kbd")
+(column-number-mode t)
+(display-time-mode t)
+(line-number-mode t)
+(show-paren-mode t)
+(menu-bar-mode nil)
 
 (cond ((fboundp 'global-font-lock-mode)
        (global-font-lock-mode t)
@@ -52,82 +52,73 @@
 
 (cond ((if window-system  t nil)
        (set-background-color "DarkSlateGrey")
-       (set-foreground-color  "Wheat")
-       (set-cursor-color      "gold1")
-       (tool-bar-mode nil)))
+       (set-foreground-color "Wheat")
+       (set-cursor-color "gold1")
+       (tool-bar-mode nil)
+       (set-language-environment 'Chinese-GB)
+       ))
 
-;;(add-hook 'c-mode-common-hook
-;;	  '(lambda () (c-toggle-auto-state t)))
+(add-hook 'c-mode-common-hook
+	  '(lambda () (c-toggle-auto-state t)))
 (add-hook 'dired-load-hook
           (lambda ()
             (load "dired-x")
             ;; Set dired-x global variables here.  For example:
             ;; (setq dired-guess-shell-gnutar "gtar")
-            ;; (setq dired-x-hands-off-my-keys nil)
             ))
-(add-hook 'dired-mode-hook
-          (lambda ()
-            ;; Set dired-x buffer-local variables here.  For example:
-	    (setq dired-omit-files-p nil)
-            ))
+;;(add-hook 'dired-mode-hook
+;;          (lambda ()
+;;            ;; Set dired-x buffer-local variables here.  For example:
+;;	    (setq dired-omit-files-p nil)
+;;            ))
 
 (require 'python-mode)
 (setq auto-mode-alist
       (cons '("\\.py$" . python-mode) auto-mode-alist))
 (setq interpreter-mode-alist
       (cons '("python" . python-mode) interpreter-mode-alist))
+(setq auto-mode-alist
+      (cons '("\\.ml[iylp]?$" . caml-mode) auto-mode-alist))
+(setq auto-mode-alist
+      (cons '("\\.xsd$" . sgml-mode) auto-mode-alist))
 
-(defun dos-unix ()
-  (interactive)
+(autoload 'caml-mode "ocaml" 
+  (interactive)  "Major mode for editing Caml code." t)
+(autoload 'camldebug "camldebug" 
+  (interactive) "Debug caml mode")
+(autoload 'run-caml "inf-caml" 
+  "Run an inferior Caml process." t)
+(if window-system (require 'caml-font))
+
+(defun dos-unix () (interactive)
   (goto-char (point-min))
   (while (search-forward "\r" nil t) (replace-match "")))
-(defun unix-dos ()
-  (interactive)
+(defun unix-dos () (interactive)
   (goto-char (point-min))
   (while (search-forward "\n" nil t) (replace-match "\r\n")))
+(defun eshell-clears-buffer ()
+  "Deletes the contents of eshell buffer, except the last prompt"
+  (interactive)
+  (save-excursion
+    (goto-char eshell-last-output-end)
+    (let ((lines (count-lines 1 (point)))
+	  (inhibit-read-only t))
+      (beginning-of-line)
+      (let ((pos (point)))
+	(if (bobp)
+	    (if (interactive-p)
+		(error "Buffer too short to truncate"))
+	  (delete-region (point-min) (point))
+	  (if (interactive-p)
+	      (message "Buffer cleared")))))))
 
-;;(require 'tramp)
 (require 'jka-compr)
 (require 'dired-tar)
-;;(require 'cubeconf-mode)
-
-(set-language-environment 'Chinese-GB)
-;;(create-fontset-from-fontset-spec
-;; "-*-courier new-normal-r-*-*-13-*-*-*-c-*-fontset-chinese,
-;;      chinese-gb2312:-*-MS Song-normal-r-*-*-16-*-*-*-c-*-gb2312*-*,
-;;      chinese-big5-1:-*-MingLiU-normal-r-*-*-16-*-*-*-c-*-big5*-*,
-;;      chinese-big5-2:-*-MingLiU-normal-r-*-*-16-*-*-*-c-*-big5*-*" t)
-;;(setq default-frame-alist
-;;      (append
-;;       '((font . "fontset-chinese"))
-;;       default-frame-alist))
-;;(defun load-gb ()
-;;  (set-language-environment 'chinese-gb) ; or 'chinese-big5
-;;  (set-selection-coding-system 'cn-gb-2312-dos)
-;;  (set-terminal-coding-system 'cn-gb-2312-dos)
-;;;;  (set-w32-system-coding-system 'cn-gb-2312-dos) ; for Chinese Windows users
-;;  )
-;;(load-gb); or substitute big5 for gb in load-gb and call (load-big5)
-
-
-(custom-set-variables
-  ;; custom-set-variables was added by Custom -- don't edit or cut/paste it!
-  ;; Your init file should contain only one such instance.
- '(Info-enable-edit nil)
- '(auto-compression-mode t nil (jka-compr))
- '(case-fold-search t)
- '(current-language-environment "Chinese-GB")
- '(default-input-method "chinese-py-punct")
- '(diary-file "~/.diary" t)
- '(dired-listing-switches "-alv")
- '(global-font-lock-mode t nil (font-lock))
- '(show-paren-mode t nil (paren)))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom -- don't edit or cut/paste it!
-  ;; Your init file should contain only one such instance.
- )
+(require 'cubeconf-mode)
+(require 'iptables-save-mode)
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
+(put 'erase-buffer 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (put 'erase-buffer 'disabled nil)
 (put 'rmail 'disabled t)
