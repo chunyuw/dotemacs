721478373a4b1bb150ca9d94ce29243184f06352
Chunyu Wang <spr@db.hit.edu.cn>
2003-05-18 08:33:59 +0000

    some changes and new features.

--- .emacs.el
+++ .emacs.el
@@ -11,35 +11,35 @@
 (global-set-key [f11] 'compile)
 (global-set-key [f12] 'gdb)
 
-(global-set-key "\C-x\C-b" 'bs-show) ;; `ibuffer' or `electric-buffer-list' other choices
+(global-set-key "\C-x\C-b" 'electric-buffer-list) ; `ibuffer' , `electric-buffer-list' or `bs-show'
 (global-set-key "\C-x\C-j" 'dired-jump)
-(global-set-key "\C-xk" 'kill-buffer-directly)
+(global-set-key "\C-xk"    'kill-buffer-directly)
 (global-set-key "\C-c\C-o" 'occur)
 (global-set-key "\C-c\C-v" 'view-mode)
 (global-set-key "\C-c\C-z" 'pop-global-mark)
 (global-set-key "\C-z" 'set-mark-command)
-(global-set-key "\M-/" 'hippie-expand) ;;dabbrev-expand
+(global-set-key "\M-/" 'hippie-expand)		 ; dabbrev-expand
+(global-set-key "\M-o" 'other-window)            ; M-o
 (global-set-key [(home)] 'beginning-of-buffer)
 (global-set-key [(end)] 'end-of-buffer)
 
 ;;(setq text-mode-hook 'turn-on-auto-fill)
-;;(setq inhibit-startup-message t)
-;;(setq default-major-mode 'text-mode)
+(setq inhibit-startup-message t)
+(setq default-major-mode 'text-mode)
 (setq uniquify-buffer-name-style 'forward)
-(setq bs-default-configuration 'all) ;; all, files or files-and-scratch
+(setq ido-save-directory-list-file nil)
+(setq resize-mini-windows nil)
+(setq bs-default-configuration 'all) ; all, files or files-and-scratch
 (setq display-time-day-and-date t)
 (setq next-line-add-newlines nil)
 (setq display-time-24hr-format t)
 (setq require-final-newline t)
 (setq user-mail-address "dddkk@sina.com")
-(setq dired-listing-switches "-avl")  ;;Set it by hand with `C-u s'.
+(setq dired-listing-switches "-avl")	; Set it by hand with `C-u s'.
 (setq user-full-name "Chunyu")
-;;(setq gdb-command-name "gdb ")
 (setq compile-command "make ")
 (setq diary-file "~/.diary")
-;;(setq scroll-step 0)
 (setq bookmark-save-flag 1)
-
 
 (setq version-control t)
 (setq kept-old-versions 2)
@@ -52,55 +52,48 @@
 (column-number-mode t)
 (display-time-mode t)
 (show-paren-mode t)
-(menu-bar-mode nil)
-(iswitchb-mode)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(ido-mode t)
+
+(set-terminal-coding-system 'chinese-iso-8bit)
+(set-language-environment 'Chinese-GB)
+
+(setq hippie-expand-try-functions-list
+      '(try-expand-line
+        try-expand-line-all-buffers
+        try-expand-list
+        try-expand-list-all-buffers
+        try-expand-dabbrev
+        try-expand-dabbrev-visible
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-file-name
+        try-complete-file-name-partially
+        try-complete-lisp-symbol
+        try-complete-lisp-symbol-partially
+        try-expand-whole-kill))
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 (set-register ?q '(file . "/ftp:chunyu@itrb#8021:/"))
 (setenv "DISPLAY" "chunyu:0")
 
-(set-terminal-coding-system 'chinese-iso-8bit)
-;;(set-keyboard-coding-system 'chinese-iso-8bit)
-(set-language-environment 'Chinese-GB)
+(add-hook 'dired-load-hook (lambda () (load "dired-x")))
 
-(setq hippie-expand-try-functions-list
-      '(try-expand-dabbrev
-        try-expand-dabbrev-visible
-        try-expand-dabbrev-all-buffers
-        try-expand-dabbrev-from-kill
-        try-expand-list
-        try-expand-list-all-buffers
-        try-expand-line
-        try-expand-line-all-buffers
-        try-complete-file-name
-        try-complete-file-name-partially
-	try-complete-lisp-symbol
-        try-complete-lisp-symbol-partially
-        try-expand-whole-kill))
-
-(cond ((if window-system  t nil)
+(cond (window-system
        (set-background-color "DarkSlateGrey")
        (set-foreground-color "Wheat")
-       (set-cursor-color "gold1")
-       (tool-bar-mode nil)
-       ))
-
-(add-hook 'dired-load-hook
-	  (lambda ()
-            (load "dired-x")
-            ;; Set dired-x global variables here.  For example:
-            ;; (setq dired-guess-shell-gnutar "gtar")
-            ))
+       (set-cursor-color "gold1")))
 	      (setq default-frame-alist
 (autoload 'python-mode "python-mode" "Python editing mode." t)
 (setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
+(autoload 'css-mode "css-mode.el" "CSS editing mode" t)
+(setq auto-mode-alist (cons '("\\.css\\'" . css-mode) auto-mode-alist))
 
-(defun dos-unix () (interactive)
-  (goto-char (point-min))
+(defun dos-unix () (interactive) (goto-char (point-min))
   (while (search-forward "\r" nil t) (replace-match "")))
-(defun unix-dos () (interactive)
-  (goto-char (point-min))
+(defun unix-dos () (interactive) (goto-char (point-min))
   (while (search-forward "\n" nil t) (replace-match "\r\n")))
 (defun kill-buffer-directly () (interactive) (kill-buffer nil))
 		("\\.s?html?\\'" . html-helper-mode)
@@ -112,19 +105,10 @@
 (put 'dired-find-alternate-file 'disabled nil)
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
+(put 'narrow-to-page 'disabled nil)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(canlock-password "f6d9e4eb8360cf2cd5db9d5d0f83f7f09ac74767"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq canlock-password "f6d9e4eb8360cf2cd5db9d5d0f83f7f09ac74767")
 
 (put 'upcase-region 'disabled nil)
 (put 'erase-buffer 'disabled nil)
