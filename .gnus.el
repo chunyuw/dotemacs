2c0adde16feba77a789fcdc040781787343f4ded
Chunyu Wang <spr@db.hit.edu.cn>
2003-06-06 05:37:29 +0000

    update most of the content.

--- .gnus.el
+++ .gnus.el
@@ -1,24 +1,56 @@
-;; $Id$  -*- Emacs-Lisp -*-
-;; Chunyu's .gnus.el is created on db.hit.edu.cn.    Chunyu.<dddkk@sina.com> 
-
+;; -*- Emacs-lisp -*-
 
 (setq gnus-select-method '(nntp "news.cn99.com"))
+(setq gnus-message-archive-group
+      '((if (message-news-p) "misc-news"
+          (concat "mail." (format-time-string "%Y-%m")))))
+(setq gnus-group-name-charset-group-alist
+      '((".*" . gb2312)))
+
 (setq gnus-secondary-select-methods '((nnml "private")))
 (add-to-list 'gnus-secondary-select-methods '(nntp "localhost"))
-;;(add-to-list 'gnus-secondary-select-methods '(nntp "news.bentium.com"))
-;;(add-to-list 'gnus-secondary-select-methods '(nntp "freenews.netfront.net"))
-;;(add-to-list 'gnus-secondary-select-methods '(nntp "news.so-net.com.hk"))
-;;(add-to-list 'gnus-secondary-select-methods '(nntp "news.zixia.com"))
-(add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
+;; nntp hosts: news.bentium.com, freenews.netfront.net,
+;;  news.zixia.com, news.webking.cn, news.newsfan.net
+;;国内: news.cnnb.net 国外: news.ind.mh.se
 
+(add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
+(add-hook 'mail-citation-hook 'sc-cite-original)
+(add-hook 'message-mode-hook
+	  (lambda () (setq fill-column 72) (turn-on-auto-fill)))
+
+(setq nnmail-crosspost nil)
 (setq nnmail-split-methods
       '(("emacs-devel" "^\\(To:\\|[Cc]c:\\).*emacs-devel@gnu.org.*")
-	("zope" "^\\(To:\\|[Cc][Cc]:\\).*zope@zope.org.*")
-	("bbs" "^From: .*\\.bbs@bbs.hit.edu.cn")
+	("zope" "^\\(To:\\|[Cc]c:\\).*zope@zope.org")
+	("bbs" "^From:.*\\(@bbs\\|@smth\\)")
+	("wife" "^From:.*\\(pacz\\|tccz\\)")
+	("classmate" "^From:.*\\(moomooo\\|beautifulbamboo\\)")
+	("classmate" "^Form:.*hit.edu.cn")
 	("lab" "^From:.*db.hit.edu.cn")
-	("default" "")))
-;;(setq mail-sources
-;;      '( 
-;;	(file :path "/var/spool/mail/spr")
-;;	(pop :server "pop3.sina.com.cn" :user "dddkk" :password "sssss")
-;;	))
+	("misc" "^\\([Cc]c\\|To\\):.*\\(chunyu\\|spr\\|dddkk@sina.com\\)")
+	;;("junk" "^From:.*sinamx.sina.com.cn")
+	("junk" "")))
+
+(setq gnus-posting-styles
+      '((".*" 
+	 (signature-file "~/.signature/default")
+	 (name "Wang Chunyu")
+	 (address "o.oO@Oo.o"))
+	("^nntp\\+localhost.*"  
+	 (signature-file "~/.signature/localnews")
+	 (name "王春宇")
+	 (address "spr@db.hit.edu.cn"))
+	("^cn\\..*\\|^nntp.*\\(webking\\|newsfan\\)"
+	 (signature-file "~/.signature/cnnews")
+	 (name "进化的鱼")
+	 (address "o.oO@Oo.o"))
+	("^nnml\\+private.*\\|mail"
+	 (signature-file "~/.signature/mail")
+	 (name "王春宇")
+	 (address "dddkk@sina.com"))))
+
+
+(eval-after-load "mm-decode"
+  '(progn
+     (add-to-list 'mm-discouraged-alternatives "text/html")
+     (add-to-list 'mm-discouraged-alternatives "text/richtext")))
