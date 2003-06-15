1ab778218cf4c2f7449a30cb0185941d87ab74c1
Chunyu Wang <spr@db.hit.edu.cn>
2003-06-15 08:22:04 +0000

    new style and more things

--- .emacs.el
+++ .emacs.el
@@ -1,5 +1,7 @@
 ;; $Id$  -*- Emacs-Lisp -*-
-;; Chunyu's .emacs.el is created on db.hit.edu.cn.    Chunyu.<dddkk@sina.com> 
+;; Chunyu's .emacs.el is created on 2001/12/11 on db.hit.edu.cn. <dddkk@sina.com>
+
+;; Time-stamp: <2003-06-15 Sun 13:52:31 spr on db.hit.edu.cn>.
 
 (global-set-key [f1] 'cvs-examine)
 (global-set-key [f5] 'undo)
@@ -13,42 +15,117 @@
 (global-set-key "\C-x\C-b" 'electric-buffer-list)
 (global-set-key "\C-x\C-j" 'dired-jump)
 (global-set-key "\C-xk" 'kill-buffer-directly)
-(global-set-key "\C-xm" 'man-follow)
 (global-set-key "\C-c\C-o" 'occur)
 (global-set-key "\C-c\C-v" 'view-mode)
 (global-set-key "\C-c\C-z" 'pop-global-mark)
+(global-set-key "\C-\\" 'toggle-truncate-lines)
 (global-set-key "\C-z" 'set-mark-command)
-(global-set-key "\M-/" 'hippie-expand)           ; dabbrev-expand
-(global-set-key "\C-\\" 'dictionary-search)
-(global-set-key "\M-o" 'other-window)            ; M-o
+(global-set-key "\M-/" 'hippie-expand)	; dabbrev-expand
+(global-set-key "\M-o" 'other-window)	; M-o
 (global-set-key [(home)] 'beginning-of-buffer)
 (global-set-key [(end)] 'end-of-buffer)
 
-(setq inhibit-startup-message t)
-(setq default-major-mode 'text-mode)
-(setq uniquify-buffer-name-style 'forward)
-(setq ido-save-directory-list-file nil)
-(setq resize-mini-windows nil)
-(setq display-time-day-and-date t)
-(setq next-line-add-newlines nil)
-(setq display-time-24hr-format t)
-(setq require-final-newline t)
-(setq user-mail-address "dddkk@sina.com")
-(setq dired-listing-switches "-avl")    ;`C-u s'
-(setq user-full-name "Chunyu Wang")
-(setq compile-command "make ")
-(setq diary-file "~/.diary")
-(setq bookmark-save-flag 1)
-(setq dired-recursive-copies t)
-(setq dired-recursive-deletes t)
-(setq Man-notify-method 'pushy)
-(setq dictionary-server "192.168.1.194")
+(define-prefix-command 'ctl-xm-map)
+(global-set-key "\C-xm" 'ctl-xm-map)
+(define-key ctl-xm-map "s" 'dictionary-search)
+(define-key ctl-xm-map "c" 'boxquote-shell-command)
+(define-key ctl-xm-map "f" 'boxquote-describe-function)
+(define-key ctl-xm-map "i" 'boxquote-insert-file)
+(define-key ctl-xm-map "k" 'boxquote-describe-key)
+(define-key ctl-xm-map "p" 'boxquote-paragraph)
+(define-key ctl-xm-map "r" 'boxquote-region)
+(define-key ctl-xm-map "t" 'boxquote-title)
+(define-key ctl-xm-map "u" 'boxquote-unbox)
+(define-key ctl-xm-map "v" 'boxquote-describe-variable)
+(define-key ctl-xm-map "y" 'boxquote-yank)
+(define-key ctl-xm-map "e" 'cvs-examine)
+(define-key ctl-xm-map "m" 'man-follow)
+(define-key ctl-xm-map "g" 'run-scheme)
 
-(setq version-control t)
-(setq kept-old-versions 2)
-(setq kept-new-versions 5)
-(setq delete-old-versions t)
-(setq backup-directory-alist '(("" . "~/tmp")))
+(setq inhibit-startup-message t
+      default-major-mode 'text-mode
+      next-line-add-newlines nil
+      require-final-newline t
+      resize-mini-windows nil
+      bookmark-save-flag 1
+      track-eol t
+      compile-command "make "
+      Man-notify-method 'pushy
+      ido-save-directory-list-file nil
+      uniquify-buffer-name-style 'forward
+
+      dired-listing-switches "-avl"	;`C-u s'
+      dired-recursive-copies t
+      dired-recursive-deletes t
+
+      display-time-24hr-format t
+      display-time-day-and-date t
+      display-time-use-mail-icon t
+
+      mail-signature-file "~/.sig/default"
+      mail-user-agent 'message-user-agent
+      user-full-name "Chunyu Wang"
+      user-mail-address "dddkk@sina.com"
+
+      quack-default-program "guile"
+      quack-fontify-style nil
+
+      time-stamp-active t
+      time-stamp-warn-inactive t
+      time-stamp-format "%:y-%02m-%02d %3a %02H:%02M:%02S %u on %s"
+
+      diary-file "~/.diary"
+      calendar-latitude +45.75
+      calendar-longitude +126.63
+      calendar-location-name "Harbin"
+      calendar-remove-frame-by-deleting t
+      calendar-week-start-day 1
+      mark-diary-entries-in-calendar t
+      appt-issue-message nil
+      mark-holidays-in-calendar nil
+      view-calendar-holidays-initially nil
+
+      version-control t
+      kept-old-versions 2
+      kept-new-versions 5
+      delete-old-versions t
+      backup-directory-alist '(("" . "~/tmp"))
+
+      font-lock-maximum-decoration t
+      font-lock-global-modes '(not shell-mode text-mode)
+      font-lock-verbose t
+      font-lock-maximum-size '((t . 1048576) (vm-mode . 5250000))
+
+      hippie-expand-try-functions-list
+      '(try-expand-line
+	try-expand-line-all-buffers
+	try-expand-list
+	try-expand-list-all-buffers
+	try-expand-dabbrev
+	try-expand-dabbrev-visible
+	try-expand-dabbrev-all-buffers 
+	try-expand-dabbrev-from-kill
+	try-complete-file-name
+	try-complete-file-name-partially
+	try-complete-lisp-symbol 
+	try-complete-lisp-symbol-partially
+	try-expand-whole-kill)
+
+      dictionary-server "192.168.1.194"
+      canlock-password "a6763075ef97955033c40069155a4ef7b1d67fee")
+(setq help-at-pt-display-when-idle 
+(set-register ?e '(file . "~/.emacs.el"))
+(set-register ?g '(file . "~/.gnus.el"))
+(setq-default kill-whole-line t)
+
+(set-register ?q '(file . "/ftp:chunyu@itrb#8021:/"))
+(set-register ?z '(file . "/usr/local/share/zope/var/Z2.log"))
+(set-language-environment 'Chinese-GB)
+(set-keyboard-coding-system 'chinese-iso-8bit)
+(set-terminal-coding-system 'chinese-iso-8bit)
+
+
+(set-selection-coding-system 'chinese-iso-8bit)
 ;; (prefer-coding-system 'chinese-iso-8bit)
 (global-font-lock-mode t)
 (auto-compression-mode t)
@@ -59,34 +136,6 @@
 (tool-bar-mode -1)
 (ido-mode t)
 
-(set-terminal-coding-system 'chinese-iso-8bit)
-(set-keyboard-coding-system 'chinese-iso-8bit)
-(set-language-environment 'Chinese-GB)
-
-(setq hippie-expand-try-functions-list
-      '(try-expand-line
-        try-expand-line-all-buffers
-        try-expand-list
-        try-expand-list-all-buffers
-        try-expand-dabbrev
-        try-expand-dabbrev-visible
-        try-expand-dabbrev-all-buffers
-        try-expand-dabbrev-from-kill
-        try-complete-file-name
-        try-complete-file-name-partially
-        try-complete-lisp-symbol
-        try-complete-lisp-symbol-partially
-        try-expand-whole-kill))
-
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-(set-register ?e '(file . "~/.emacs.el"))
-(set-register ?q '(file . "/ftp:chunyu@itrb#8021:/"))
-(set-register ?z '(file . "/usr/local/share/zope/var/Z2.log"))
-(setenv "DISPLAY" "chunyu:0")
-(setq emacs-wiki-directories  '("~/00PAGE/m/Wiki"))
-(setq canlock-password "f6d9e4eb8360cf2cd5db9d5d0f83f7f09ac74767")
-
 (ido-mode 1)
 (add-hook 'dired-load-hook (lambda () (load "dired-x")))
 
@@ -95,12 +144,14 @@
   (set-foreground-color "Wheat")
   (set-cursor-color "gold1"))
 	      (setq default-frame-alist
+		    (append '((top . 0) (left . 0)
+			      (width . 111) (height . 46)
+			      (font . "9x15"))
+
+(autoload 'big5togb-region "big5togb" "Big5 to GB2312" t)
+(autoload 'todo-insert-item "todo-mode" "Add TODO item." t)
 (autoload 'todo-mode "todo-mode" "Major mode for editing TODO lists." t)
 (autoload 'todo-show "todo-mode" "Show TODO items." t)
-(autoload 'todo-insert-item "todo-mode" "Add TODO item." t)
-(autoload 'big5togb-region "big5togb" "Big5 to GB2312" t)
-(autoload 'python-mode "python-mode" "Python editing mode." t)
-(autoload 'css-mode "css-mode" "CSS editing mode" t)
 (autoload 'htmlize-buffer "htmlize.el" "HTMLize mode" t)
 (setq auto-mode-alist 
 ;; (autoload 'folding-mode          "folding" "Folding mode" t)
@@ -113,17 +164,23 @@
   (while (search-forward "\n" nil t) (replace-match "\r\n")))
 (defun kill-buffer-directly () (interactive) (kill-buffer nil))
 		("\\.s?html?\\'" . html-helper-mode)
+		("\\.css\\'" . css-mode))
 (require 'compile)
+	      auto-mode-alist))
 (require 'jka-compr)
 (require 'uniquify)
-(require 'dired-tar)
 
 (put 'dired-find-alternate-file 'disabled nil)
 (put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
-(put 'narrow-to-region 'disabled nil)
 (put 'narrow-to-page 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
 (put 'set-goal-column 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(put 'rmail 'disabled t)
+(add-to-list 'load-path "~/.emacs.d/elisp")
+(load-file "~/.emacs_smtp")
+
+;; .emacs.el ends here.
 (put 'upcase-region 'disabled nil)
 (put 'erase-buffer 'disabled nil)
 (put 'rmail 'disabled t)
--- .gnus.el
+++ .gnus.el
@@ -1,58 +1,144 @@
 ;; $Id$  -*- Emacs-Lisp -*-
 ;; Chunyu's .gnus.el is created on db.hit.edu.cn.    Chunyu.<dddkk@sina.com> 
 
+;; Time-stamp: <2003-06-15 Sun 16:15:14 spr on db.hit.edu.cn>.
+
 ;; NNTP Hosts: 
-;;  news.bentium.com, news.zixia.com, freenews.netfront.net, news.ind.mh.se
+;;  freenews.netfront.net, news.ind.mh.se
+;;  news.gmane.org, news.gnus.org, news.cis.dfn.de
+;;  news.freshmeat.net, news.uni-duisburg.de
 ;;  news.webking.cn, news.newsfan.net, new.cnnb.net
 
-(setq gnus-select-method '(nntp "news.cn99.com"))
-(setq gnus-secondary-select-methods '((nnml "private")))
-(add-to-list 'gnus-secondary-select-methods '(nntp "localhost"))
+(setq gnus-select-method '(nntp "news.cn99.com")
+      gnus-secondary-select-methods '((nnml "private") (nntp "localhost"))
+      gnus-auto-select-first nil
+      gnus-auto-select-subject 'first
+      gnus-inhibit-startup-message t
+      gnus-read-newsrc-file nil
+      gnus-save-newsrc-file nil
+      gnus-asynchronous t     ; *No* delay when I hop betwixt articles
+      gnus-summary-display-while-building 50 ; Show us what you're doing, every fifty articles
 
-(setq gnus-message-archive-group
+      gnus-group-name-charset-group-alist '((".*" . gb2312))
+
+      gnus-message-archive-group
       '((if (message-news-p) "misc-news"
-          (concat "mail." (format-time-string "%Y-%m")))))
+	  (concat "mail." (format-time-string "%Y-%m"))))
 
-(setq gnus-group-name-charset-group-alist
-      '((".*" . gb2312)))
+      gnus-posting-styles
+      '((".*" 
+	 (signature-file "~/.sig/default")
+	 (name "Wang Chunyu")
+	 (address "o.oO@Oo.o"))
+	("^nntp\\+localhost.*"  
+	 (signature-file "~/.sig/localnews")
+	 (name "王春宇")
+	 (address "spr@db.hit.edu.cn"))
+	("^cn\\..*\\|^nntp.*\\(webking\\|newsfan\\)"
+	 (signature-file "~/.sig/cnnews")
+	 (name "进化的鱼")
+	 (address "o.oO@Oo.o"))
+	("^nnml\\+private.*\\|mail"
+	 (signature-file "~/.sig/mail")
+	 (name "王春宇")
+	 (address "dddkk@sina.com"))))
 
-(add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
-(add-hook 'mail-citation-hook 'sc-cite-original)
-(add-hook 'message-mode-hook
-	  (lambda () (setq fill-column 72) (turn-on-auto-fill)))
+(setq sc-attrib-selection-list nil
+      sc-auto-fill-region-p nil
+      sc-blank-lines-after-headers 1
+      sc-cite-blank-lines-p nil
+      sc-electric-references-p nil
+      sc-fixup-whitespace-p t
+      sc-preferred-header-style 5
+      sc-use-only-preference-p nil
+      sc-preferred-attribution-list
+      '("sc-lastchoice" "x-attribution" "initials" "firstname" "sc-consult" "lastname"))
 
-(setq nnmail-crosspost nil)
-(setq nnmail-split-methods
+(setq mail-self-blind t
+      message-from-style 'angles
+      message-kill-buffer-on-exit t
+      ;;message-make-forward-subject-function 'message-forward-subject-fwd
+      message-sendmail-envelope-from nil
+      message-signature t
+      message-signature-file "~/.sig/default"
+      message-forward-as-mime nil
+      message-forward-ignored-headers
+      "^Content-Transfer-Encoding:\\|^X-\\|^Path\\|^Organization\\|^Message-ID\\|^References\\|^User-Agent\\|^Cancel\\|^NNTP\\|^Lines\\|^Received\\|^Reply-To\\|^Precedence\\|^Return-Path")
+
+
+(setq nnmail-crosspost nil
+      nnmail-split-methods
       '(("emacs-devel" "^\\(To:\\|[Cc]c:\\).*emacs-devel@gnu.org.*")
+	("guile-user" "^\\(To:\\|[Cc]c:\\).*guile-user@gnu.org")
+	("guile-sources" "^\\(To:\\|[Cc]c:\\).*guile-sources@gnu.org")
 	("zope" "^\\(To:\\|[Cc]c:\\).*zope@zope.org")
 	("bbs" "^From:.*\\(@bbs\\|@smth\\)")
 	("wife" "^From:.*\\(pacz\\|tccz\\)")
-	("classmate" "^From:.*\\(moomooo\\|beautifulbamboo\\)")
+	("classmate" "^From:.*\\(moomooo\\|beautifulbamboo\\|dangtx\\|kexin\\|zjj259900\\)")
+	("classmate" "^From:.*\\(jellycart\\|limoo\\)")
 	("classmate" "^Form:.*hit.edu.cn")
 	("lab" "^From:.*db.hit.edu.cn")
 	("misc" "^\\([Cc]c\\|To\\):.*\\(chunyu\\|spr\\|dddkk@sina.com\\)")
 	("junk" "^From:.*sinamx.sina.com.cn")
 	("junk" "")))
 
-(setq gnus-posting-styles
-      '((".*" 
-	 (signature-file "~/.signature/default")
-	 (name "Wang Chunyu")
-	 (address "o.oO@Oo.o"))
-	("^nntp\\+localhost.*"  
-	 (signature-file "~/.signature/localnews")
-	 (name "王春宇")
-	 (address "spr@db.hit.edu.cn"))
-	("^cn\\..*\\|^nntp.*\\(webking\\|newsfan\\)"
-	 (signature-file "~/.signature/cnnews")
-	 (name "进化的鱼")
-	 (address "o.oO@Oo.o"))
-	("^nnml\\+private.*\\|mail"
-	 (signature-file "~/.signature/mail")
-	 (name "王春宇")
-	 (address "dddkk@sina.com"))))
+(add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
+(add-hook 'mail-citation-hook 'sc-cite-original)
+(add-hook 'message-mode-hook (lambda () (setq fill-column 72) (turn-on-auto-fill)))
 
 (eval-after-load "mm-decode"
   '(progn
      (add-to-list 'mm-discouraged-alternatives "text/html")
      (add-to-list 'mm-discouraged-alternatives "text/richtext")))
+
+(defun my-sc-header-fn ()
+  "<bbdbname/aliasname/sc-author> (attrib) wrote (on <date>):"
+  (condition-case nil
+      (let* ((sc-mumble nil)   ; returned by sc-mail-field if no match
+	     (sc-name (or (sc-mail-field "sc-author")
+			  sc-default-author-name))
+	     (email (or (sc-mail-field "sc-from-address") "noaddress"))
+	     ;; Gnus does not have first two functions.
+	     (name (or (if (fboundp 'bbdb/vm-alternate-full-name)
+			   (bbdb/vm-alternate-full-name email))
+		       (if (fboundp 'my-mail-lookup-in-aliases)
+			   (my-mail-lookup-in-aliases email))
+		       sc-name))
+	     (date (sc-mail-field "date"))
+	     (string "")
+	     (attrib (sc-mail-field "sc-attribution"))
+	     (attrib (if (string= attrib "") "" (format " (%s)" attrib)))
+	     datelist zonesecs zonesign zonehrs zonemins dateint)
+	;; Don't bother with dates for Gnus posts.
+	(if (eq major-mode 'message-mode)
+	    (setq date nil
+		  ;; Decided no point quoting e-mail address.
+		  ;; string (format " <%s> wrote" email)
+		  string " wrote"))
+	(if date
+	    (setq datelist (parse-time-string date)
+		  string   (format-time-string ; UT
+			    "%a, %d %b %Y at %R %z"
+			    (apply 'encode-time datelist) t)
+		  zonesecs (car (last datelist))))
+	;; Stop here (eg set zonesecs nil) if happy with times in UT.
+	;; `parse-time-string' gives offset from UT in seconds.
+	;; Convert to (sign)(hours)(mins).
+	(if zonesecs			; nil if could not be parsed
+	    (setq zonesign (if (< zonesecs 0) "-" "+")
+		  zonesecs (abs zonesecs)
+		  zonehrs  (floor (/ zonesecs 3600))
+		  zonemins (/ (- zonesecs (* zonehrs 3600)) 60)
+		  datelist (butlast datelist) ; remove TZ info
+		  dateint  (apply 'encode-time datelist)
+		  string   (format-time-string "%a, %d %b %Y at %R" dateint)
+		  string   (format " wrote (on %s %s%.2d%.2d)"
+				   string zonesign zonehrs zonemins)))
+	(insert "\n" sc-reference-tag-string name attrib string ":\n"))
+    (error (insert "\nNONAME wrote:\n" )
+	   (message "Error in my-sc-header-fn"))))
+
+(add-hook 'sc-load-hook
+	  '(lambda () (if (boundp 'sc-rewrite-header-list)
+			  (add-to-list 'sc-rewrite-header-list '(my-sc-header-fn)))))
+
