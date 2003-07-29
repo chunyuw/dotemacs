df021f21318f7c4de7d43464d9f516776619f00a
Chunyu Wang <spr@db.hit.edu.cn>
2003-07-29 02:47:47 +0000

    remove comment.

--- .emacs_bbdb.el
+++ .emacs_bbdb.el
@@ -2,8 +2,8 @@
 ;; Chunyu's .emacs_bbdb.el is created on 2003/07/26 on db.cs.hit.edu.cn. 
 ;; Chunyu <chunyu@hit.edu.cn>.
 
-;;(require 'bbdb)
-;;(bbdb-initialize)
-;;(add-hook 'gnus-startup-hook 'bbdb-insinuate-gnus)
-;;(bbdb-insinuate-message)
+(require 'bbdb)
+(bbdb-initialize)
+(add-hook 'gnus-startup-hook 'bbdb-insinuate-gnus)
+(bbdb-insinuate-message)
 
