4a6fbebd9d9115f208fe97e6216a65642cd009fb
Chunyu Wang <spr@db.hit.edu.cn>
2003-07-31 04:01:57 +0000

    setup for bbdb.

--- .emacs_bbdb.el
+++ .emacs_bbdb.el
@@ -5,5 +5,14 @@
 (require 'bbdb)
 (bbdb-initialize)
 (add-hook 'gnus-startup-hook 'bbdb-insinuate-gnus)
-(bbdb-insinuate-message)
+
+(setq bbdb-north-american-phone-numbers-p nil
+      bbdb-check-zip-codes-p nil
+      bbdb-electric-p t
+      bbdb-use-pop-up nil		; 'horiz
+      bbdb-pop-up-target-lines 1
+      bbdb-elided-display-fields '(net)
+      bbdb-elided-display t
+      bbdb-offer-save nil
+)
 
