880a66df9844375f8c532daf34001b7b4a2fb3ca
Chunyu Wang <spr@db.hit.edu.cn>
2003-08-16 10:21:24 +0000

    use daemon.

--- .gnus.el
+++ .gnus.el
@@ -161,10 +161,10 @@
      (setq mm-discouraged-alternatives '("text/html")
 	   mm-automatic-display (remove "text/html" mm-automatic-display))))
 
-;; (gnus-demon-init)
-;; (gnus-demon-add-rescan)
-;; (gnus-demon-add-disconnection)
-;; (gnus-demon-add-scanmail)
+(gnus-demon-init)
+(gnus-demon-add-rescan)
+(gnus-demon-add-disconnection)
+(gnus-demon-add-scanmail)
 
 (unless window-system 
   (require 'gnus-sum)
