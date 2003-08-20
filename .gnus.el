9d0cc43b0b8c4a7e90f03ece75b2cad7b757e875
Chunyu Wang <spr@db.hit.edu.cn>
2003-08-20 10:27:32 +0000

    postgres-general@postgres.org list.

--- .gnus.el
+++ .gnus.el
@@ -138,12 +138,14 @@
       nnmail-split-fancy-match-partial-words t
       nnmail-split-fancy
       '(|
-	(from "sinamx.sina.com.cn" "misc.misc")
+	(to "Dddkk \\<dddkk@sina\\.com\\>" "misc.junk")
+	;;(from "sinamx.sina.com.cn" "misc.junk")
 	(any "emacs-devel@gnu.org" "list.emacs-devel")
 	(any "guile-user@gnu.org" "list.guile-user")
 	(any "guile-sources@gnu.org" "list.guile-sources")
 	(any "ding@gnus.org" "list.ding")
 	(any "fetchmail-friends" "list.fetchmail")
+	(any "pgsql-general@postgresql.org" "list.pg-general")
 	(any "zope@zope.org" "list.zope")
 	(any "985101.*@db.cs.hit.edu.cn" "classmate.985101")
 	(to "chunyu@hit.edu.cn\\|dddkk@sina.com\\|spr@db"
