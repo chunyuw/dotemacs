83c629f97bf31c28ab429d180f69eb6230a0abe1
Chunyu Wang <spr@db.hit.edu.cn>
2003-08-25 12:08:51 +0000

    new file for customize info.

---
+++ .emacs_custom.el
@@ -0,0 +1,18 @@
+;; $Id$  -*- Emacs-Lisp -*-
+;; Chunyu's .emacs_custom.el is created on 2003/08/25 on db.hit.edu.cn.
+;; Chunyu <chunyu@hit.edu.cn>.
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+
+;; Chunyu's .emacs_custom.el ends here.
