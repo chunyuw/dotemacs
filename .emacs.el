5a12d85b635269c44f28e27a8e1558cc40513689
Chunyu Wang <spr@db.hit.edu.cn>
2003-10-28 01:22:38 +0000

    *** empty log message ***

--- .emacs.el
+++ .emacs.el
@@ -68,7 +68,7 @@
       bookmark-default-file "~/.emacs.d/.emacs.bmk"
 
       compilation-scroll-output t
-      compile-command "make -k"
+      compile-command "make -k "
 
       dired-listing-switches "-avl"     ; `C-u s'
       dired-recursive-copies t
