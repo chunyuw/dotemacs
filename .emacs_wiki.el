c23a74929834ad874179a7b108ad6a739fce5fa2
Chunyu Wang <spr@db.hit.edu.cn>
2003-11-17 09:32:14 +0000

    new for emacs-wiki.el.

---
+++ .emacs_wiki.el
@@ -0,0 +1,9 @@
+;; $Id$  -*- Emacs-Lisp -*-
+;; Chunyu's .emacs_wiki.el is created on 2003/11/17 on db.cs.hit.edu.cn.
+;; Chunyu <chunyu@hit.edu.cn>.
+
+(require 'emacs-wiki)
+
+(setq emacs-wiki-charset-default "gb2312")
+
+;; Chunyu's .emacs_wiki.el ends here.
