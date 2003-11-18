3c259f2e0b8fb42baf81d7b5f835827f94142dba
Chunyu Wang <spr@db.hit.edu.cn>
2003-11-18 12:21:34 +0000

    修改 Emacs Wiki 缺省设置。

--- .emacs_wiki.el
+++ .emacs_wiki.el
@@ -5,5 +5,22 @@
 (require 'emacs-wiki)
 
 (setq emacs-wiki-charset-default "gb2312")
+(setq emacs-wiki-coding-default 'gb2312)
+
+(setq emacs-wiki-index-page "WikiIndex")
+
+(setq emacs-wiki-pretty-changelogs t)
+
+(setq emacs-wiki-publishing-header 
+      (concat "<!--#let title_or_id=\"'<lisp>(emacs-wiki-page-title)</lisp>'\"-->\n"
+	      "<!--#var standard_html_header-->\n<!--#/let-->\n\n"))
+
+(setq emacs-wiki-publishing-footer
+      "\n<!--#var standard_html_footer-->\n")
+
+(setq emacs-wiki-maintainer "&dtml.url-contact;?subject=[Lack of Item]")
+;;(setq emacs-wiki-maintainer "mailto:spr@db.cs.hit.edu.cn?Subject=[Lack of Item]")
+
+(add-hook 'emacs-wiki-mode-hook 'auto-fill-mode)
 
 ;; Chunyu's .emacs_wiki.el ends here.
