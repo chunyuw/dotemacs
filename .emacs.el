46eb04bf595aaf834ab8c0e85621095ba4a2abd0
Chunyu Wang <spr@db.hit.edu.cn>
2004-06-22 01:53:28 +0000

    *** empty log message ***

--- .emacs.el
+++ .emacs.el
@@ -5,6 +5,7 @@
 (global-set-key [f12] 'gdb)
 (global-set-key [home] 'beginning-of-buffer)
 (global-set-key [end] 'end-of-buffer)
+(global-set-key [(control tab)] 'bbdb-complete-name)
 (global-unset-key [(insert)])
 
 (global-set-key "\C-x\C-b" 'bs-show)
