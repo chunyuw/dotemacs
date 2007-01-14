4a9dd6de80b356cbfedf0ff98da412cab73d1c92
Chunyu Wang <chunyu@hit.edu.cn>
2007-01-14 03:12:24 +0000

    *** empty log message ***

--- init.el
+++ init.el
@@ -719,6 +719,7 @@ comment char"
   (set-face-attribute 'minibuffer-prompt nil :foreground "chocolate1")
   (set-face-attribute 'mode-line nil :foreground "black" :background "wheat" :box nil)
   (set-face-attribute 'mode-line-inactive nil :foreground "grey90" :background "grey30" :box '(:color "grey50"))
+  (set-face-attribute 'mode-line-highlight nil :box '(:line-width 1 :color "grey40"))
   (set-face-attribute 'region nil :background "grey21")
   (set-face-attribute 'trailing-whitespace nil :background "SeaGreen1")
   (copy-face 'default 'font-lock-warning-face)
