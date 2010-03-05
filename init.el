a56883a4145ee5f4487d669ff91a6a246a2cfb29
Chunyu Wang <cymacs@gmail.com>
2010-03-05 13:22:13 +0800

    Batch checkin for .emacs.d (2010-03-05 13:22 on chunyu-PC).

--- init.el
+++ init.el
@@ -681,20 +681,6 @@ Frame must be declared as an environment."
 (let ((s nil)) (dotimes (i 18) (setq s (append '(?\~ ?\  ?\ ) s)))
      (aset standard-display-table ?\f (vconcat '(?\~ ?\~) s '(?\~ ?\~ ?\~))))
 
-;; Customizations ;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 ;; Local Variables:
 ;; mode: emacs-lisp
 ;; coding: gbk-unix
