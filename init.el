f4fe4f56dbfebcd6f406582c02b3121632831855
Chunyu Wang <cymacs@gmail.com>
2010-09-22 00:09:42 +0800

    Batch checkin for .emacs.d (2010-09-22 00:09 on CYMACS-PC).

--- init.el
+++ init.el
@@ -313,6 +313,7 @@
 
 ;; Anything ;;
 (setq anything-su-or-sudo "sudo"
+      anything-c-locate-command "locate -i %s"
       anything-for-files-prefered-list
       '(anything-c-source-ffap-line
 	anything-c-source-ffap-guesser
