cbcbea6389c1abe74008b4a2b62c4b6beff5e4ed
Chunyu Wang <cymacs@gmail.com>
2010-10-03 23:33:34 +0800

    Batch checkin for .emacs.d (2010-10-03 23:33 on CYMACS-PC).

---
+++ others/init-w32.el
@@ -0,0 +1,9 @@
+
+(set-language-environment    'Chinese-GB18030)
+(set-default-coding-systems  'chinese-gb18030)
+(set-keyboard-coding-system  'chinese-gb18030)
+(set-terminal-coding-system  'chinese-gb18030)
+(set-clipboard-coding-system 'chinese-gb18030)
+(set-selection-coding-system 'chinese-gb18030)
+
+(server-mode 1)
