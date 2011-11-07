73886a59c65492d8b4dfb28b7728fba28b5393c0
Chunyu Wang <cymacs@gmail.com>
2011-11-07 10:22:36 +0800

    Batch checkin for .emacs.d (2011-11-07 10:22 on NCCY-PC).

--- others/init-nil.el
+++ others/init-nil.el
@@ -3,6 +3,10 @@
 (display-time-mode 1)
 (setq image-load-path nil)
 
+;; (setq message-send-mail-function 'message-send-mail-with-sendmail
+;;       sendmail-program "msmtp" mail-host-address "hit.edu.cn"
+;;       user-full-name "Chunyu Wang" user-mail-address "chunyu@hit.edu.cn")
+
 ;; (set-language-environment    'UTF-8)
 ;; (set-default-coding-systems  'utf-8)
 ;; (set-keyboard-coding-system  'utf-8)
