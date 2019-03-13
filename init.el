ed9ecaa882fdd0737b36e56f358cb8b9def2b293
Chunyu Wang <cymacs@gmail.com>
2019-03-13 12:51:50 +0800

    Batch commit. (2019-03-13 12:51 on DESKTOP-D8FIKCN)

--- init.el
+++ init.el
@@ -340,7 +340,7 @@
       reftex-plug-into-AUCTeX t
       reftex-section-levels
       '(("part" . 0) ("chapter" . 1) ("section" . 2) ("subsection" . 3)
-	("frametitle" . 4) ("subsubsection" . 4) ("paragraph" . 5)
+	("frametitle" . 3) ("subsubsection" . 4) ("paragraph" . 5)
 	("subparagraph" . 6) ("addchap" . -1) ("addsec" . -2)))
 
 (setq font-latex-fontify-script 'invisible
