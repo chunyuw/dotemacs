a91747dc27d79959b442595e37593a4ecbf33127
Chunyu Wang <cymacs@gmail.com>
2023-11-16 09:32:24 +0800

    Batch commit. (2023-11-16 09:32 on DESKTOP-BQO60FM)

--- init.el
+++ init.el
@@ -105,7 +105,6 @@
       savehist-ignored-variables '(file-name-history))
 
 (set-register ?e '(file . "~/.emacs.d/init.el"))
-(set-register ?i '(file . "/sshx:ii:/var/www/iilab/automata"))
 
 (fset 'yes-or-no-p 'y-or-n-p)
 
