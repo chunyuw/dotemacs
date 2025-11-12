4146ce6bd41d0bf1664ac43c01c0bfc9063079ce
Chunyu Wang <chunyu@hit.edu.cn>
2025-11-12 19:32:54 +0800

    Batch commit. (2025-11-12 19:32 on cy-MacBookAir.local)

--- init.el
+++ init.el
@@ -529,7 +529,8 @@
  '(compilation-window-height 4)
  '(global-eldoc-mode nil)
  '(package-selected-packages
-   '(auctex cdlatex dired-gitignore helm magit markdown-mode tempel valign))
+   '(auctex cdlatex dired-gitignore edit-indirect helm magit markdown-mode tempel
+            valign))
  '(safe-local-variable-values '((TeX-command-extra-options . "-shell-escape")))
  '(tramp-auto-save-directory "~/.tmp"))
 (custom-set-faces
